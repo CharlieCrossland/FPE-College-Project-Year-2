c63eae05c94af456361ff8a60b6aaceaea9f55cf
CharlieCrossland <cro24003689@heartofyorkshire.ac.uk>
2025-12-07 02:16:36 +0000

    Added Rob's first person controller
    I also commented areas and added code to find references independently. Input handler script can now also be referenced without having to create a variable in each script.

---
+++ Assets/Scripts/Player/FirstPersonController.cs
@@ -0,0 +1,147 @@
+using UnityEngine;
+using Unity.Cinemachine;
+
+// add components needed for script to work
+// prevents compile errors
+[RequireComponent(typeof(CharacterController))]
+public class FirstPersonController : MonoBehaviour
+{
+    [Header("Movement Speeds")]
+    [SerializeField] private float walkSpeed = 3.0f;
+    [SerializeField] private float sprintMultiplier = 2.0f;
+
+    [Header("Jump Parameters")]
+    [SerializeField] private float jumpForce = 5.0f;
+    [SerializeField] private float gravityMultiplier = 1.0f;
+
+    [Header("Look Parameters")]
+    [SerializeField] private float mouseSensitivity = 0.1f;
+    [SerializeField] private float upDownLookRange = 80f;
+
+    [Header("References")]
+    [SerializeField] private CharacterController characterController;
+    [SerializeField] private CinemachineCamera mainCamera;
+    [SerializeField] private PlayerInputHandler playerInputHandler;
+
+    private Vector3 currentMovement;
+    private float verticalRotation;
+
+    // directly manipulate the variable
+    private float CurrentSpeed => walkSpeed * (playerInputHandler.SprintTriggered ? sprintMultiplier : 1);
+
+    private void Awake()
+    {
+        CheckReferences();
+    }
+
+    void CheckReferences()
+    {
+        // avoid anything missing that could break the game
+        // a really shitty way to do this but i wanted to learn how to use returns
+        // bite me
+        bool allReferencesGrabbed = CheckNull();
+
+        if (allReferencesGrabbed == true)
+        {
+            return;
+        }
+        if (allReferencesGrabbed == false)
+        {
+            characterController = GetComponent<CharacterController>();
+            mainCamera = GetComponentInChildren<CinemachineCamera>();
+            playerInputHandler = PlayerInputHandler.Instance;
+        }
+    }
+
+    bool CheckNull()
+    {
+        // variables may be set as null/are missing
+        // these are failsafes incase they do
+        if (characterController || mainCamera || playerInputHandler == null)
+        {
+            return false;
+        }
+        return true;
+    }
+
+    void Start()
+    {
+        SetCursor();
+    }
+
+    void SetCursor()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    void Update()
+    {
+        HandleMovement();
+        HandleRotation();
+    }
+    
+    private Vector3 CalculateWorldDirection()
+    {
+        // use the players input to find a local position in x and y
+        // use the local position to then find the global space position
+        // return that to the HandleMovement
+        Vector3 inputDirection = new (playerInputHandler.MovementInput.x, 0, playerInputHandler.MovementInput.y);
+        Vector3 worldDirection = transform.TransformDirection(inputDirection);
+        return worldDirection.normalized;
+    }
+    
+    private void HandleJumping()
+    {
+        // use characterController.isGrounded
+        // depreciate the use of raycasts to check ground
+        // should be better for performance in long run
+        if(characterController.isGrounded)
+        {
+            currentMovement.y = -0.5f;
+
+            if(playerInputHandler.JumpTriggered)
+            {
+                currentMovement.y = jumpForce;
+            }
+        }
+        else
+        {
+            
+            currentMovement.y += Physics.gravity.y * gravityMultiplier * Time.deltaTime;
+        }
+    }
+    
+    private void HandleMovement()
+    {
+        Vector3 worldDirection = CalculateWorldDirection();
+        currentMovement.x = worldDirection.x * CurrentSpeed;
+        currentMovement.z = worldDirection.z * CurrentSpeed;
+
+        // i dont like the fact HandleJumping is so hidden and nested lmao
+        HandleJumping();
+        
+        characterController.Move(currentMovement * Time.deltaTime);
+    }
+    
+    private void ApplyHorizontalRotation(float rotationAmount)
+    {
+        transform.Rotate(0, rotationAmount, 0);
+    }
+    
+    private void ApplyVerticalRotation(float rotationAmount)
+    {
+        verticalRotation = Mathf.Clamp(verticalRotation - rotationAmount, -upDownLookRange, upDownLookRange);
+        mainCamera.transform.localRotation = Quaternion.Euler(verticalRotation, 0, 0);
+    }
+    
+    private void HandleRotation()
+    {
+        float mouseXRotation = playerInputHandler.RotationInput.x * mouseSensitivity;
+        float mouseYRotation = playerInputHandler.RotationInput.y * mouseSensitivity;
+        
+        ApplyHorizontalRotation(mouseXRotation);
+        ApplyVerticalRotation(mouseYRotation);        
+    }
+    
+}
